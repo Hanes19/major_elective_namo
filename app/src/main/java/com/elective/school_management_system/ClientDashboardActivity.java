8cb32cf5819b53af35f42fd1ee5f466e4496d83a
Hue <ayanosuzuki04@gmail.com>
2025-12-13 01:19:31 +0800

    new update

--- app/src/main/java/com/elective/school_management_system/ClientDashboardActivity.java
+++ app/src/main/java/com/elective/school_management_system/ClientDashboardActivity.java
@@ -26,7 +26,7 @@ public class ClientDashboardActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.db_client_first_page);
+        setContentView(R.layout.db_client_dashboard);
 
         initViews();
         setupListeners();
